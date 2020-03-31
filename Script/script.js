e505a7485e4caeaa2c7114dd203577f2922b65b8
xlArtemlx <59087414+xlArtemlx@users.noreply.github.com>
2020-04-01 00:31:32 +0300

    Initial commit

---
+++ Script/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DONContentLoaded', function() {
+    'use strict'
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a){
+        for( let i = a ; i < tabContent.length;i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+
+    };
+    hideTabContent(1);
+
+    function showTabContent(b){
+        if(tabContent.classList.contains(hide)){
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    };
+    info.addEventListener('click',function(event){
+        let target = target.event;
+        if(target && target.classList.contains('.info-header-tab')){
+            for(i = 0; i < tab.length;i++){
+                if(target === tab[i]){
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
